4919a91b50c494db4d559ebdf8380dd6ba6bcb33
AbdelaliSaadali <abdelali.saadali.rajawi.11@gmail.com>
2026-03-02 11:54:30 +0000

    Commit 4 - Add CommandLineRunner to test DAO with sample data

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import com.example.demo.entities.Product;
+import com.example.demo.repositories.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +14,13 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner start(ProductRepository productRepository) {
+		return args -> {
+			productRepository.save(new Product(null, "Computer", 3500, 10));
+			productRepository.save(new Product(null, "Printer", 1200, 5));
+			productRepository.save(new Product(null, "SmartPhone", 2500, 8));
+			productRepository.findAll().forEach(p -> System.out.println(p.toString()));
+		};
+	}
 }
